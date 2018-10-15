91e001ec7abc3cff3e474147d313f2912d6324b9
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-10-15 14:32:35 +0900

    Initial Commit

---
+++ BusanAirQualityParsing02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusanAirQualityParsing02
+//
+//  Created by D7703_17 on 2018. 10. 15..
+//  Copyright © 2018년 D7703_17. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+      override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view, typically from a nib.
+      }
+
+
+}
+
